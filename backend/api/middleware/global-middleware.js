5c867a49180eb32981d27ecb382bb4809b2ac6eb
Sushant <krishna@Krishnas-MacBook-Air.local>
2020-04-06 17:04:01 +0530

    changes done

--- backend/api/middleware/global-middleware.js
+++ backend/api/middleware/global-middleware.js
@@ -9,21 +9,16 @@ const { configureGithubStrategy } = require("./passport-github");
 const { devConfig } = require("../config/env/development");
 const session = require("express-session");
 const User = require("../resources/users/model/users.model");
- module.exports.setGlobalMiddleware = app => {
-
+module.exports.setGlobalMiddleware = app => {
   // var distDir = __dirname + "../../../dist/";
   // app.use(express.static(distDir));
 
   if (process.env.NODE_ENV === "production") {
-
     app.use(express.static("build"));
- 
- 
     app.get("*", (req, res) => {
-         res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-     });
- 
-   }
+      res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    });
+  }
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
